a971b5fed2d8169a40018276401b5fd800057d1c
NezcalSpindrake <darkcloudfleethoof@gmail.com>
2018-11-02 16:00:01 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
 var helium = 0;
 var heat = 0;
 
+//basic click function
+
 function heliumClick(number){
 	helium = helium + number;
 	document.getElementById("helium").innerHTML = helium;
 };
 
+//function used to buy heat and get cost of heat
+
 function buyHeat(){
-	var heatCost = Math.floor(10 * Math.pow(1.1,heat));
+	var heatCost = Math.floor(10 * Math.pow(1.1,heat)); //calculates cost of heat based on amount owned
 	if(helium >= heatCost){
 		heat = heat + 1;
 		helium = helium - heatCost;
@@ -18,11 +22,15 @@ function buyHeat(){
 	document.getElementById("heatCost").innerHTML = nextCost;
 };
 
+//game loop for autoclickers
+
 window.setInterval(function(){
 	
 	heliumClick(heat);
 	
 }, 1000);
+
+//save function saves to local storage using JSON string
 
 function save(){
 	
@@ -35,6 +43,8 @@ function save(){
 	localStorage.setItem("save",JSON.stringify(save));
 	
 };
+
+//load function loads from local storage using JSON parsing of the save string
 
 function load(){
 	
